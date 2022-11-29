24b4b9084486559d6624eb460ff6e6e61bcd759e
Gaguchi <gaga9393@gmail.com>
2022-11-30 00:24:00 +0400

    models

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import product
+
 # Create your views here.
+
+def index(request):
+    return render(request, "cards/index.html", {
+        "products": product.objects.all()
+    })
