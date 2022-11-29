de3b162e9aafc4fe4f2b0a1213fa43b2380f769a
Gaguchi <gaga9393@gmail.com>
2022-11-30 00:23:53 +0400

    models

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class product(models.Model):
+    name = models.CharField(max_length=64)
+    image = models.ImageField(upload_to='product_images')
+    barcode = models.CharField(max_length=64)
