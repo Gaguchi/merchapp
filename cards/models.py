f75da4dcef810b31808fc9faa131c00bed10951d
Gaguchi <gaga9393@gmail.com>
2022-12-01 15:30:48 +0400

    models

--- cards/models.py
+++ cards/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class cities(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64, unique=True)
+
+class shops(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+    city = models.ForeignKey(cities, on_delete=models.CASCADE)
+
+class brands(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+    image = models.ImageField(upload_to='brand_images')
 
 class product(models.Model):
-    name = models.CharField(max_length=64)
+    id = models.AutoField(primary_key=True)
+    brand = models.ForeignKey(brands, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64, blank=False)
     image = models.ImageField(upload_to='product_images')
-    barcode = models.CharField(max_length=64)
+    box = models.IntegerField()
+    barcode = models.ImageField(upload_to='product_barcodes')
