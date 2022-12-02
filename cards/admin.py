4ffb63b1f24c19a59af91f6d34088c4f8dcbacb3
Gaguchi <gaga9393@gmail.com>
2022-12-02 20:15:16 +0400

    shops spb

--- cards/admin.py
+++ cards/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+admin.site.register(product)
+admin.site.register(brand)
+admin.site.register(shops_SPB)
+admin.site.register(supermarkets_SPB)
+admin.site.register(perek_spb)
--- cards/models.py
+++ cards/models.py
@@ -1,24 +1,106 @@
 from django.db import models
 
 # Create your models here.
-class cities(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=64, unique=True)
 
-class shops(models.Model):
+class shops_SPB(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64)
-    city = models.ForeignKey(cities, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='shop_images')
+    url = models.URLField()
+    
+class supermarkets_SPB(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+    image = models.ImageField(upload_to='shop_images')
+    url = models.URLField()
 
-class brands(models.Model):
+class brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64)
     image = models.ImageField(upload_to='brand_images')
 
 class product(models.Model):
     id = models.AutoField(primary_key=True)
-    brand = models.ForeignKey(brands, on_delete=models.CASCADE)
+    brand = models.ForeignKey(brand, on_delete=models.CASCADE)
     name = models.CharField(max_length=64, blank=False)
     image = models.ImageField(upload_to='product_images')
     box = models.IntegerField()
     barcode = models.ImageField(upload_to='product_barcodes')
+
+class perek_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class pyaterechka_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class briz_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class vkuster_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class plovdiv_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class rosneft_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class dixy_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class prisma_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class vernyi_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class magnit_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class lenta_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class auchan_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
+    
+class ok_spb(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    plu = models.IntegerField()
+    priority = models.IntegerField(unique=True)
