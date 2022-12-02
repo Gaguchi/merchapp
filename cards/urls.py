b3d980ec7d21f33ce00c23d173e529fd7b570906
Gaguchi <gaga9393@gmail.com>
2022-12-02 20:15:14 +0400

    shops spb

--- cards/urls.py
+++ cards/urls.py
@@ -1,8 +1,25 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
 app_name="cards"
 urlpatterns = [
     path("", views.index ,name="index"),
+    path("perekspb", views.perekspb, name="perek_spb"),
+    path("peterechkaspb", views.peterechkaspb, name="peterechka_spb"),
+    path("brizspb", views.brizspb, name="briz_spb"),
+    path("vkusterspb", views.vkusterspb, name="vkuster_spb"),
+    path("plovdivspb", views.plovdivspb, name="plovdiv_spb"),
+    path("rosneftspb", views.rosneftspb, name="rosneft_spb"),
+    path("dixyspb", views.dixyspb, name="dixy_spb"),
+    path("prismaspb", views.prismaspb, name="prisma_spb"),
+    path("vernyispb", views.vernyispb, name="vernyi_spb"),
+    path("magnitspb", views.magnitspb, name="magnit_spb"),
+    path("lentaspb", views.lentaspb, name="lenta_spb"),
+    path("auchanspb", views.auchanspb, name="auchan_spb"),
+    path("okspb", views.okspb, name="ok_spb")
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cards/views.py
+++ cards/views.py
@@ -1,10 +1,75 @@
 from django.shortcuts import render
 
-from .models import product
+from .models import *
 
 # Create your views here.
 
 def index(request):
     return render(request, "cards/index.html", {
         "products": product.objects.all()
+    })
+
+def perekspb(request):
+    return render(request, "cards/perek_spb.html", {
+        "names": perek_spb.objects.all()
+    })
+
+def peterechkaspb(request):
+    return render(request, "cards/peterechka_spb.html", {
+        "names": pyaterechka_spb.objects.all()
+    })
+
+def brizspb(request):
+    return render(request, "cards/briz_spb.html", {
+        "names": briz_spb.objects.all()
+    })
+
+def vkusterspb(request):
+    return render(request, "cards/vkuster_spb.html", {
+        "names": vkuster_spb.objects.all()
+    })
+
+def plovdivspb(request):
+    return render(request, "cards/plovdiv_spb.html", {
+        "names": plovdiv_spb.objects.all()
+    })
+
+def rosneftspb(request):
+    return render(request, "cards/rosneft_spb.html", {
+        "names": rosneft_spb.objects.all()
+    })
+
+def dixyspb(request):
+    return render(request, "cards/dixy_spb.html", {
+        "names": dixy_spb.objects.all()
+    })
+
+def prismaspb(request):
+    return render(request, "cards/prisma_spb.html", {
+        "names": prisma_spb.objects.all()
+    })
+
+def vernyispb(request):
+    return render(request, "cards/vernyi_spb.html", {
+        "names": vernyi_spb.objects.all()
+    })
+
+def magnitspb(request):
+    return render(request, "cards/magnit_spb.html", {
+        "names": magnit_spb.objects.all()
+    })
+
+def lentaspb(request):
+    return render(request, "cards/lenta_spb.html", {
+        "names": lenta_spb.objects.all()
+    })
+
+def auchanspb(request):
+    return render(request, "cards/auchan_spb.html", {
+        "names": auchan_spb.objects.all()
+    })
+
+def okspb(request):
+    return render(request, "cards/ok_spb.html", {
+        "names": ok_spb.objects.all()
     })
